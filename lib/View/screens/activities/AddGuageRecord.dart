75f76b42e4bf2c71bc3363d73638c0c81710ceaa
michaelson7 <michaelnawa1998@gmail.com>
2021-11-22 16:12:44 +0200

    update 5.8

--- lib/View/screens/activities/AddGuageRecord.dart
+++ lib/View/screens/activities/AddGuageRecord.dart
@@ -145,23 +145,50 @@ class _AddGuageRecordState extends State<AddGuageRecord> {
         onTap: () {
           attachImage();
         },
-        child: imageFile != null
-            ? CircleAvatar(
-                radius: 100,
-                backgroundColor: kAccent,
-                backgroundImage: FileImage(
-                  File(imageFile.path),
-                ),
-              )
-            : CircleAvatar(
-                radius: 80,
-                backgroundColor: kAccent,
-                backgroundImage: NetworkImage(
-                  widget.gaugeRecordsModel != null
-                      ? widget.gaugeRecordsModel!.imagepath
-                      : "",
+        child: SizedBox(
+          height: 200,
+          width: 200,
+          child: Stack(
+            clipBehavior: Clip.none,
+            fit: StackFit.expand,
+            children: [
+              imageFile != null
+                  ? CircleAvatar(
+                      radius: 120,
+                      backgroundColor: kAccent,
+                      backgroundImage: FileImage(
+                        File(imageFile.path),
+                      ),
+                    )
+                  : CircleAvatar(
+                      radius: 80,
+                      backgroundColor: kAccent,
+                      backgroundImage: NetworkImage(
+                        widget.gaugeRecordsModel != null
+                            ? widget.gaugeRecordsModel!.imagepath
+                            : "",
+                      ),
+                    ),
+              Positioned(
+                bottom: 0,
+                right: -25,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    attachImage();
+                  },
+                  elevation: 2.0,
+                  fillColor: Color(0xFFF5F6F9),
+                  child: Icon(
+                    Icons.camera_alt_outlined,
+                    color: Colors.blue,
+                  ),
+                  padding: EdgeInsets.all(15.0),
+                  shape: CircleBorder(),
                 ),
               ),
+            ],
+          ),
+        ),
       ),
     );
   }
